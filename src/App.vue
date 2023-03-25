8e9cc652f1e013bb60ce66ea078188aa459f88d2
zyshu <zyshu11571@outlook.com>
2023-03-25 12:06:20 +0800

    Additional Configuration

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <HeadTitle></HeadTitle>
+    <NavView></NavView>
+  </div>
+</template>
+
+<script>
+import HeadTitle from "./components/HeadTitle.vue";
+import NavView from "./components/NavView.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeadTitle,
+    NavView
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+
+a {
+  text-decoration: none;
+  color: #000;
+}</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import { MessageBox, Notification, Loading } from 'element-ui'
+import { downloadXlsx } from './utils/downloadXlsx.js'
+
+Vue.config.productionTip = false
+// 修改this.$confirm出现问题的办法
+Vue.prototype.$confirm = MessageBox.confirm
+Vue.prototype.$notify = Notification;
+Vue.use(Loading.directive);
+
+// 将定义的表格导出下载的方法函数放进vue实例中
+Vue.prototype.downloadXlsx = downloadXlsx;
+
+new Vue({
+  render: h => h(App),
+  router,
+}).$mount('#app')
