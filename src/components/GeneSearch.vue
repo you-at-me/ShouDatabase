96f768acd9adced8c7754ffb0b5775784aa23c95
zyshu <zyshu11571@outlook.com>
2023-03-25 13:15:23 +0800

    Gene search

---
+++ src/components/GeneSearch.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'GeneSearch',
+    data() {
+      return {
+        msg: '',
+        obj: {},
+      }
+    },
+    components: {},
+    mounted() {},
+    methods: {},
+  };
+</script>
+
+<style scoped>
+
+</style>
