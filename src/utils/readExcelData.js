0a4bbf9d43fa74f563020522f8326b2e78203637
zyshu <zyshu11571@outlook.com>
2023-04-06 20:22:10 +0800

    fix bugs

--- src/utils/readExcelData.js
+++ src/utils/readExcelData.js
@@ -10,7 +10,7 @@ function transformSheets(sheets) {
   }
   console.log(tmplist); // 输出每个表格具有的总行数，文件当中有几个表格就会输出几个数
   // 输出是嵌套数组，文件当中的每个表格以数组的方式嵌套在外层数组里，并表格当中有几行数据就会把对应表格的每行数据都附在数组当中，并可以通过获取数组的长度来获取表格当中总共有几条数据。
-  console.log(content); 
+  console.log(content);
   return content;
 }
 
@@ -20,5 +20,5 @@ export default function readExcelData(url) { // url的路径直接写'/xxx.xlsx'
     const data = new Uint8Array(res.data);
     let wb = XLSX.read(data, { type: 'array' });
     return transformSheets(wb.Sheets);
-  }).catch(err => console.log(err))
+  }, err => console.log(err)).catch(err => console.log(err))
 }
