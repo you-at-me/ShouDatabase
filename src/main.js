ea4f71e6ff78052d5ad3c8f9d8fb203ed0a5ff9f
zyshu <zyshu11571@outlook.com>
2023-04-07 16:52:14 +0800

    add methods

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { MessageBox, Notification, Loading } from 'element-ui'
 import { downloadXlsx } from './utils/downloadXlsx.js'
+import { getXlsxSpecifyData } from '@/utils/xlsxSpecifyData';
 
 Vue.config.productionTip = false;
 // 修改this.$confirm出现问题的办法
@@ -12,6 +13,7 @@ Vue.use(Loading.directive);
 
 // 将定义的表格导出下载的方法函数放进vue实例中
 Vue.prototype.downloadXlsx = downloadXlsx;
+Vue.prototype.getXlsxSpecifyData = getXlsxSpecifyData;
 
 new Vue({
   render: h => h(App),
