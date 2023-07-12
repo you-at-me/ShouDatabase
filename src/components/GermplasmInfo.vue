eecece6f8326badd859d936c48ac3deab3436ce5
zyshu <zyshu11571@outlook.com>
2023-07-12 22:32:02 +0800

    add tips

--- src/components/GermplasmInfo.vue
+++ src/components/GermplasmInfo.vue
@@ -22,6 +22,7 @@ export default {
   },
   mounted() {
     console.log("flag", this.$route.query);
+    // 这里有一个小问题：虽然这里的判断参数解决了就是真正从Germplasm resources列表那块访问来的，可以携带具体的参数，然后访问到具体的那个资源信息info,也就是下述会直接走return这块逻辑。但是同样会产生一个问题：而如果是从具体的访问进去的列表那块过来访问germplasm-info,还是germplasm-info自己访问自己，将不会走这里的mounted()函数逻辑，也就不会出现默认的查询列表。而且created()函数也是一样的。除此之外，还有一个显示的小bug,就是在从其他页面访问这个germplasm-info页面路径的时候，会出现其他页面闪一下的感觉，不是那么流畅。
     if (JSON.stringify(this.$route.query) !== '{}') return;
     this.$router.push({
       name: "germplasm-ctss",
